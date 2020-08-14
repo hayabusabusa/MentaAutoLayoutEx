99ee1f3f16a20bbf449b35f0928f39600c8c7d74
hayabusabusa <hayabusabusaSY@gmail.com>
2020-08-14 16:16:12 +0900

    Add projects

---
+++ MentaAutoLayoutEx/Exercises/First/FirstViewController.swift
@@ -0,0 +1,23 @@
+//
+//  FirstViewController.swift
+//  MentaAutoLayoutEx
+//
+//  Created by Shunya Yamada on 2020/08/10.
+//  Copyright © 2020 Shunya Yamada. All rights reserved.
+//
+
+import UIKit
+
+final class FirstViewController: UIViewController {
+    
+    // MARK: IBOutlet
+    
+    // MARK: Properties
+    
+    // MARK: Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "Ex1"
+    }
+}
---
+++ MentaAutoLayoutEx/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MentaAutoLayoutEx
+//
+//  Created by Shunya Yamada on 2020/08/10.
+//  Copyright © 2020 Shunya Yamada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
