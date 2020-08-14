6c9ca292501ac9fd92f1d8b95b4a2b5131566d83
hayabusabusa <hayabusabusaSY@gmail.com>
2020-08-14 18:09:06 +0900

    Add Second View Controller and remove auto layout constraints

---
+++ MentaAutoLayoutEx/Exercises/Second/SecondViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SecondViewController.swift
+//  MentaAutoLayoutEx
+//
+//  Created by Shunya Yamada on 2020/08/14.
+//  Copyright © 2020 Shunya Yamada. All rights reserved.
+//
+
+import UIKit
+
+final class SecondViewController: UIViewController {
+    
+    // MARK: IBOutlet
+    
+    // MARK: Properties
+    
+    // MARK: Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "Ex2"
+    }
+}
