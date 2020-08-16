ffc782883ab8cfdfeddad36831f8e3693514674b
hayabusabusa <hayabusabusaSY@gmail.com>
2020-08-17 01:20:56 +0900

    Add fourth view controller

---
+++ MentaAutoLayoutEx/Common/View/Button.swift
@@ -0,0 +1,63 @@
+//
+//  Button.swift
+//  MentaAutoLayoutEx
+//
+//  Created by Shunya Yamada on 2020/08/17.
+//  Copyright © 2020 Shunya Yamada. All rights reserved.
+//
+
+import UIKit
+
+class Button: UIButton {
+
+    // MARK: IBInspectable
+    
+    @IBInspectable var cornerRadius: CGFloat = 0
+    @IBInspectable var borderColor: UIColor = .clear
+    @IBInspectable var borderWidth: CGFloat = 0
+
+    // MARK: Properties
+    
+    // MARK: Overrides
+    
+    override var isEnabled: Bool {
+        didSet {
+            alpha = isEnabled ? 1 : 0.4
+        }
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        commonInit()
+    }
+
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        commonInit()
+
+        if let title = title(for: state) {
+            setAttributedTitle(NSAttributedString(string: title,
+                                                  attributes: [NSAttributedString.Key(String(kCTLanguageAttributeName)): "ja"]),
+                               for: .normal)
+        }
+    }
+
+    // MARK: Initializer
+    
+    private func commonInit() {
+        isExclusiveTouch = true
+        layer.masksToBounds = true
+        layer.cornerRadius = cornerRadius
+        layer.borderColor = borderColor.cgColor
+        layer.borderWidth = borderWidth
+    }
+}
---
+++ MentaAutoLayoutEx/Exercises/Fourth/FourthViewController.swift
@@ -0,0 +1,23 @@
+//
+//  FourthViewController.swift
+//  MentaAutoLayoutEx
+//
+//  Created by Shunya Yamada on 2020/08/16.
+//  Copyright © 2020 Shunya Yamada. All rights reserved.
+//
+
+import UIKit
+
+final class FourthViewController: UIViewController {
+    
+    // MARK: IBOutlet
+    
+    // MARK: Properties
+    
+    // MARK: Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "Ex4"
+    }
+}
