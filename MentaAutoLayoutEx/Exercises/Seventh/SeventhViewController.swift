1a1997929417dbfad934b3d1bff047c5795e134b
hayabusabusa <hayabusabusaSY@gmail.com>
2020-08-21 16:14:55 +0900

    Add seventh view controller

---
+++ MentaAutoLayoutEx/Exercises/Seventh/SeventhViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SeventhViewController.swift
+//  MentaAutoLayoutEx
+//
+//  Created by 山田隼也 on 2020/08/21.
+//  Copyright © 2020 Shunya Yamada. All rights reserved.
+//
+
+import UIKit
+
+final class SeventhViewController: UIViewController {
+    
+    // MARK: IBOutlet
+    
+    // MARK: Properties
+    
+    // MARK: Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
